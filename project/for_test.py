10267fda9b1ffe67611533fb99cb8bf57d029391
rokrokss <q0115643@kaist.ac.kr>
2018-11-28 07:40:36 +0900

    used list comprehension for mention-finding in testset

--- project/for_test.py
+++ project/for_test.py
@@ -1,73 +1,97 @@
 import re
 import time
 
+sentence="dsaf CrimeStat II: A Spatial Statistics for the Analysis of Crime Incident Locations patial Statistic CrimeStat II: A Spatial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations"
+sent = sentence.split()
+print(sent[5:10])
+print(sent[9])
+print(sent[10])
 
-def encode(sub, sent):
-    subwords, sentwords = sub.split(), sent.split()
-    res = [0 for _ in sentwords]    
-    for i, word in enumerate(sentwords[:-len(subwords) + 1]):
-        if all(x == y for x, y in zip(subwords, sentwords[i:i + len(subwords)])):
-            for j in range(len(subwords)):
-                res[i + j] = 1
-    return res
-
-def encode2(sub, sent):
-	p = re.compile(sub)
-	lst=[1 for i in range(len(sub.split()))]
-	vect=[ lst if items == '__match_word' else 0 for items in re.sub(p,'__match_word',sent).split()]
-	vectlstoflst=[[vec] if isinstance(vec,int) else vec for vec in vect]
-	flattened = [val for sublist in vectlstoflst for val in sublist]
-	return flattened
+def encode4(sub, sent):
+	subwords, sentwords = sub.split(), sent.split()
+	sub_pat = ('<MT-B>' + ' <MT-I>' * (len(subwords)-1)).strip()
+	sent = sent.replace(sub, sub_pat)
+	res = ['B' if w=='<MT-B>' else 'I' if w=='<MT-I>' else '_' for w in sent.split()]
+	return res
 
 
-def label_substring(sentence, mention, prev_label_sequence, data_set_id):
-    label_sequence = prev_label_sequence
-    for i, word in enumerate(sentence[:-len(mention) + 1]):
-        if all(x == y for x, y in zip(mention, sentence[i:i + len(mention)])):
-            if label_sequence[i] is '_':
-                label_sequence[i] = 'B-' + str(data_set_id)                
-            for j in range(len(mention)-1):
-                label_sequence[i + j + 1] = 'I'
-    return label_sequence
+# def encode(sub, sent):
+#     subwords, sentwords = sub.split(), sent.split()
+#     res = [0 for _ in sentwords]    
+#     for i, word in enumerate(sentwords[:-len(subwords) + 1]):
+#         if all(x == y for x, y in zip(subwords, sentwords[i:i + len(subwords)])):
+#             for j in range(len(subwords)):
+#                 res[i + j] = 1
+#     return res
+
+# def encode2(sub, sent):
+# 	p = re.compile(sub)
+# 	lst=[1 for i in range(len(sub.split()))]
+# 	vect=[lst if items == '__match_word' else 0 for items in re.sub(p,'__match_word',sent).split()]
+# 	vectlstoflst=[[vec] if isinstance(vec,int) else vec for vec in vect]
+# 	flattened = [val for sublist in vectlstoflst for val in sublist]
+# 	return flattened
 
 
-def encode3(sentences, mention_list):
-	label_sequence_list = []
-	for sentence in sentences:
-		label_sequence = ['_' for _ in sentence]
-		for data_set_id, mentions in mention_list:
-			#print(data_set_id)
-			for mention in mentions:
-				label_sequence = label_substring(sentence, mention, label_sequence, data_set_id)
-		label_sequence_list.append(label_sequence)
-	return label_sequence_list
+# def label_substring(sentence, mention, prev_label_sequence, data_set_id):
+#     label_sequence = prev_label_sequence
+#     for i, word in enumerate(sentence[:-len(mention) + 1]):
+#         if all(x == y for x, y in zip(mention, sentence[i:i + len(mention)])):
+#             if label_sequence[i] is '_':
+#                 label_sequence[i] = 'B-' + str(data_set_id)                
+#             for j in range(len(mention)-1):
+#                 label_sequence[i + j + 1] = 'I'
+#     return label_sequence
+
+
+# def encode3(sentences, mention_list):
+# 	label_sequence_list = []
+# 	for sentence in sentences:
+# 		label_sequence = ['_' for _ in sentence]
+# 		for data_set_id, mentions in mention_list:
+# 			#print(data_set_id)
+# 			for mention in mentions:
+# 				label_sequence = label_substring(sentence, mention, label_sequence, data_set_id)
+# 		label_sequence_list.append(label_sequence)
+# 	return label_sequence_list
 
 
 
-search_word = 'CrimeStat II: A Spatial Statistics'
-sentence="dsaf for the Analysis of Crime Incident Locations patial Statistic CrimeStat II: A Spatial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations"
+# search_word = 'CrimeStat II: A Spatial Statistics'
+# sentence="dsaf CrimeStat II: A Spatial Statistics for the Analysis of Crime Incident Locations patial Statistic CrimeStat II: A Spatial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations patial Statistics Program for the Analysis of Crime Incident Locations"
 
-mention = search_word.split()
-sentences = sentence.split()
-start = time.time()
+# mention = search_word.split()
+# sentences = sentence.split()
+# start = time.time()
 
-start = time.time()
-for i in range(100000):
-	encode(search_word, sentence)
-print(encode(search_word, sentence))
-spent = time.time() - start
-print(spent)
+# start = time.time()
+# for i in range(100000):
+# 	encode(search_word, sentence)
+# print(encode(search_word, sentence))
+# spent = time.time() - start
+# print(spent)
 
-start = time.time()
-for i in range(100000):
-	encode2(search_word, sentence)
-print(encode2(search_word, sentence))
-spent = time.time() - start
-print(spent)
+# start = time.time()
+# for i in range(100000):
+# 	encode2(search_word, sentence)
+# print(encode2(search_word, sentence))
+# spent = time.time() - start
+# print(spent)
 
-start = time.time()
-for i in range(100000):
-	encode3([sentences], [[1, [mention]]])
-print(encode3([sentences], [[1, [mention]]]))
-spent = time.time() - start
-print(spent)
+# start = time.time()
+# for i in range(100000):
+# 	encode3([sentences], [[1, [mention]]])
+# print(encode3([sentences], [[1, [mention]]]))
+# spent = time.time() - start
+# print(spent)
+
+# start = time.time()
+# for i in range(100000):
+# 	encode4(search_word, sentence)
+# print(encode4(search_word, sentence))
+# spent = time.time() - start
+# print(spent)
+
+
+
+
--- project/parser.py
+++ project/parser.py
@@ -71,72 +71,52 @@ def unify_hyphens(text):
 
 
 def label_substring(sentence, mention, label_sequence, data_set_id):
+    found_cnt = 0
     for i, word in enumerate(sentence[:-len(mention) + 1]):
         if all(x == y for x, y in zip(mention, sentence[i:i + len(mention)])):
             if label_sequence[i] is '_':
+                found_cnt += 1
                 label_sequence[i] = 'B-' + str(data_set_id)                
             for j in range(len(mention)-1):
                 label_sequence[i + j + 1] = 'I'
-    return label_sequence
+    return label_sequence, found_cnt
 
 
-def label_substring_test(sentence, mention, label_sequence, labeled, data_set_id, pub_date, data_set_date_dict):
-    for i, word in enumerate(sentence[:-len(mention) + 1]):
-        if all(x == y for x, y in zip(mention, sentence[i:i + len(mention)])):
-            curr_data_set_date = data_set_date_dict[data_set_id]
-            if label_sequence[i] is '_':
-                label_sequence[i] = 'B-' + str(data_set_id)
-            elif 'I' in label_sequence[i]:
-                continue
-            else: # B-id
-                prev_data_set_date = data_set_date_dict[int(label_sequence[i][2:])]
-                if prev_data_set_date > curr_data_set_date:
-                    continue
-                else:
-                    label_sequence[i] = 'B-' + str(data_set_id)
-            for j in range(len(mention)-1):
-                label_sequence[i + j + 1] = 'I-' + str(data_set_id)
-            labeled = 'Y'
-            print(label_sequence)
-    return label_sequence, labeled
-
-
-# [[data_set_id, formatted_mention_list]]
-# [sentences]
 def encode(sentences, mention_list):
     label_sequence_list = []
+    found_cnt = 0
     for sentence in sentences:
         label_sequence = ['_' for _ in sentence]
         for data_set_id, mentions in mention_list:
             for mention in mentions:
-                label_sequence = label_substring(sentence, mention, label_sequence, data_set_id)
+                label_sequence, cnt = label_substring(sentence, mention, label_sequence, data_set_id)
+                found_cnt += cnt
         label_sequence_list.append(label_sequence)
-    return label_sequence_list
+    return label_sequence_list, found_cnt
 
 
-def encode_test(sentences, data_set_mention_dict, pub_date, data_set_date_dict):
-    label_sequence_list = []
-    labeled_list = []
-    for sentence in sentences:
-        label_sequence = ['_' for _ in sentence]
-        labeled = 'N'
-        for data_set_id, mention_list in data_set_mention_dict.items():
-            if pub_date < data_set_date_dict[data_set_id]:
-                continue
-            for mention in mention_list:
-                label_sequence, labeled = label_substring_test(sentence, mention, label_sequence, labeled, data_set_id, pub_date, data_set_date_dict)
-        label_sequence = ['I' if 'I' in l else l for l in label_sequence]
-        label_sequence_list.append(label_sequence)
-        labeled_list.append(labeled)
-    return label_sequence_list, labeled_list
+def encode_test(raw_text, data_set_mention_info, pub_date):
+    label_sequence = ['_' for _ in range(len(raw_text.split()))]
+    for date, data_set_id, mention_list in data_set_mention_info:   # date이 큰 것부터(최신부터) 들어오도록 정렬되어 있음
+        if date < pub_date:
+            for mention_info in mention_list:
+                mention, raw_mention = mention_info[0], mention_info[1]
+                substitute_pattern = '<MT-B>' + ' <MT-I>' * (len(mention)-1)
+                replaced_text = raw_text.replace(raw_mention, substitute_pattern)
+                begin_mark = 'B-' + str(data_set_id)
+                res = [begin_mark if w=='<MT-B>' else 'I' if w=='<MT-I>' else '_' for w in replaced_text.split()]
+                label_sequence = [prev_l if curr_l=='_' else curr_l if prev_l=='_' else 'I' if prev_l=='I' or curr_l=='I' else prev_l for prev_l, curr_l in zip(label_sequence, res)]
+    return label_sequence
 
 
 def extract_formatted_data(formatted_publications, citation_dict):
     output = []
+    found_cnt = 0
     for publication_id, sentences in tqdm(formatted_publications.items(), total=len(formatted_publications)):
         if publication_id in citation_dict:
             mention_list = citation_dict[publication_id] # [[data_set_id, formatted_mention_list]]
-            label_sequence_list = encode(sentences, mention_list)
+            label_sequence_list, cnt = encode(sentences, mention_list)
+            found_cnt += cnt
             for sentence, label_sequence in zip(sentences, label_sequence_list):
                 output.append({'publication_id': publication_id,
                                'sentence': sentence,
@@ -149,24 +129,42 @@ def extract_formatted_data(formatted_publications, citation_dict):
                                'sentence': sentence,
                                'label_sequence': label_sequence,
                                'labeled': 'Y'})
+    print("found mentions: ", end='')
+    print(found_cnt)
     return output
 
 
-def extract_formatted_data_test(formatted_publications, data_set_mention_dict, pub_date_dict, data_set_date_dict):
-    # data_set_mention_dict[data_set_id] = [data_set_id, formatted_mention_list]
+def extract_formatted_data_test(formatted_publications, data_set_mention_info, pub_date_dict):
     output = []
-    for publication_id, sentences in tqdm(formatted_publications.items(), total=len(formatted_publications)):
-        label_sequence_list, labeled_list = encode_test(sentences, data_set_mention_dict, pub_date_dict[publication_id], data_set_date_dict)
-        for sentence, label_sequence, labeled in zip(sentences, label_sequence_list, labeled_list):
+    found_cnt = 0
+    for publication_id, [sentences, raw_text] in tqdm(formatted_publications.items(), total=len(formatted_publications)):
+        label_sequence = encode_test(raw_text, data_set_mention_info, pub_date_dict[publication_id])
+        startidx = 0
+        for sentence in sentences:
+            sub_label_sequence = label_sequence[startidx:startidx+len(sentence)]
+            startidx += len(sub_label_sequence)
+            if sub_label_sequence[0] == 'I':
+                for i, l in enumerate(sub_label_sequence):
+                    if l == 'I':
+                        sub_label_sequence[i] = '_'
+                    else:
+                        break
+            labeled = 'N'
+            if len(set(sub_label_sequence)) != 1:
+                labeled = 'Y'
             output.append({'publication_id': publication_id,
-                            'sentence': sentence,
-                            'label_sequence': label_sequence,
-                            'labeled': labeled})
+                           'sentence': sentence,
+                           'label_sequence': label_sequence,
+                           'labeled': labeled})
+        for l in label_sequence:
+            if 'B' in l:
+                found_cnt += 1
+    print("found mentions: ", end='')
+    print(found_cnt)
     return output
 
 
 def train_set_parser():
-    vocab = set()
     # set path to data_set_citations.json
     data_set_citations_json_path = "../data/input/data_set_citations.json"
     citation_dict = dict()
@@ -189,7 +187,6 @@ def train_set_parser():
                 words = []
                 for sentence in sentences:
                     words += word_tokenize(sentence)
-                    vocab.update(words)
                 formatted_mention_list.append(words)
             if publication_id in citation_dict:
                 citation_dict[publication_id].append([data_set_id, formatted_mention_list])
@@ -202,7 +199,7 @@ def train_set_parser():
     # set path to publications.json
     publications_json_path = "../data/input/publications.json"
     formatted_publications = dict()
-    print("Tokenizing publications.json file...")
+    print("Tokenizing publication files...")
     # open the publications.json file
     with open(publications_json_path) as json_publication_file:
         # parse it as JSON
@@ -214,12 +211,6 @@ def train_set_parser():
             title = publication_info.get( "title", None )
             title.encode('ascii','ignore')
             text_file_name = publication_info.get( "text_file_name", None )
-            unique_identifier = publication_info.get( "unique_identifier", None ) # id가 bbk로 시작하면 pub_date은 None임
-            if 'bbk' not in unique_identifier:
-                pub_date = publication_info.get( "pub_date", None )
-            else:
-                pub_date = 'NA'
-            pub_date.encode('ascii','ignore')
             # get raw text
             raw_text = ''
             txt_file_path = publication_txt_path_prefix + text_file_name
@@ -227,6 +218,8 @@ def train_set_parser():
                 for line in txt_file:
                     stripped_line = line.strip()
                     raw_text += ' ' + stripped_line
+                    if len(stripped_line.split()) <= 7:
+                        raw_text += '<stop>'    # marking for sentence boundary in split_into_sentences() function
             raw_text.encode('ascii','ignore')
             raw_text = unify_hyphens(raw_text)
             # add to formatted_publications dictionary
@@ -234,8 +227,8 @@ def train_set_parser():
             sentences = split_into_sentences(raw_text)
             for sentence in sentences:
                 words = word_tokenize(sentence)
-                vocab.update(words)
-                formatted_text_list.append(words)
+                if len(words) >= 5:
+                    formatted_text_list.append(words)
             formatted_publications[publication_id] = formatted_text_list 
     # tag mentions in publication text and write in csv file
     with open('./formatted-data/rcc_corpus_train.csv', 'w') as csvfile:
@@ -254,18 +247,12 @@ def train_set_parser():
         print("Writing on new csv file...", end='')
         writer.writerows(output)
         print("DONE")
-    # write collected vocabulary to a file, line by line
-    with open("./formatted-data/rcc_vocab.txt", 'w') as vocabfile:
-        vocab = list(vocab)
-        for w in vocab:
-            vocabfile.write("%s\n" % w)
 
 
 def test_set_parser(data_set_path, output_filename):
     # set path to data_set_citations.json
     data_sets_json_path = "../data/input/data_sets.json"
-    data_set_mention_dict = dict()
-    data_set_date_dict = dict()
+    data_set_mention_info = []
     pub_date_dict = dict()
     print("Loading data_sets.json file...")
     # open the publications.json file
@@ -289,7 +276,7 @@ def test_set_parser(data_set_path, output_filename):
             name = unify_hyphens(name)
             name = re.sub('\\\\\\\"', '\"', name)
             name_words = word_tokenize(name)
-            formatted_mention_list.append(name_words)
+            formatted_mention_list.append([name_words, list_to_string((name_words))])
             for mention in mention_list:
                 mention.encode('ascii','ignore')
                 mention = unify_hyphens(mention)
@@ -298,9 +285,12 @@ def test_set_parser(data_set_path, output_filename):
                 words = []
                 for sentence in sentences:
                     words += word_tokenize(sentence)
-                formatted_mention_list.append(words)
-            data_set_date_dict[data_set_id] = date
-            data_set_mention_dict[data_set_id] = formatted_mention_list
+                if all(c.islower() for c in sentence) and len(words) <= 2:
+                    continue    # to avoid pronoun mentions like 'data', 'time'
+                else:
+                    formatted_mention_list.append([words, list_to_string(words)])
+            data_set_mention_info.append([date, data_set_id, formatted_mention_list])
+    data_set_mention_info.sort(key=lambda x: int(x[0]), reverse=True)
     # set prefix to formatted publication txt files
     formatted_txt_path_prefix = "./formatted-data/"
     # set path to publications.json
@@ -333,15 +323,20 @@ def test_set_parser(data_set_path, output_filename):
                 for line in txt_file:
                     stripped_line = line.strip()
                     raw_text += ' ' + stripped_line
+                    if len(stripped_line.split()) <= 7:
+                        raw_text += '<stop>'    # marking for sentence boundary in split_into_sentences() function
             raw_text.encode('ascii','ignore')
             raw_text = unify_hyphens(raw_text)
             # add to formatted_publications dictionary
             formatted_text_list = []
+            chopped_raw_text = ''
             sentences = split_into_sentences(raw_text)
             for sentence in sentences:
                 words = word_tokenize(sentence)
-                formatted_text_list.append(words)
-            formatted_publications[publication_id] = formatted_text_list 
+                if len(words) >= 5:
+                    formatted_text_list.append(words)
+                    chopped_raw_text += ' ' + list_to_string(words)
+            formatted_publications[publication_id] = [formatted_text_list, chopped_raw_text.strip()] 
             pub_date_dict[publication_id] = pub_date
     # tag mentions in publication text and write in csv file
     output_filepath = './formatted-data/' + output_filename
@@ -357,14 +352,14 @@ def test_set_parser(data_set_path, output_filename):
             quoting=csv.QUOTE_ALL)
         writer.writeheader()
         print("Tagging pre-found dataset mentions in publications...")
-        output = extract_formatted_data_test(formatted_publications, data_set_mention_dict, pub_date_dict, data_set_date_dict)
+        output = extract_formatted_data_test(formatted_publications, data_set_mention_info, pub_date_dict)
         print("Writing on new csv file...", end='')
         writer.writerows(output)
         print("DONE")
 
 
 # for trainset
-# train_set_parser()
+train_set_parser()
 # for devset
 publication_txt_path_prefix = './dev_data/'
 output_filename = 'rcc_corpus_dev.csv'
